b21dc8e5a0fe3c06064a9fd8695137c9226f3432
MohebYarahmadi <moheb.yarahmadi@gmail.com>
2026-09-01 10:23:17 +0330

    Retrieving Data all

--- app/core/settings.py
+++ app/core/settings.py
@@ -140,3 +140,20 @@ SPECTACULAR_SETTINGS = {
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
 }
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django.db.backends": {
+            "level": "DEBUG",   # Log SQL queries
+            "handlers": ["console"],
+        },
+    },
+}
